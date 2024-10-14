6bcbcc909eb98ceee36d847953277d32fa2d8da9
Jim Galvan <58128571+JimGalvan@users.noreply.github.com>
2024-10-13 20:44:56 -0700

    update csrf

--- NexMenu/settings/prod.py
+++ NexMenu/settings/prod.py
@@ -21,4 +21,5 @@ DATABASES = {
     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
-CSRF_TRUSTED_ORIGINS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://nexmenu-production.up.railway.app/']
+
