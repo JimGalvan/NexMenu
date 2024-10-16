b294309806f4a8b558b1ca93f283160dd4414d89
Jim Galvan <58128571+JimGalvan@users.noreply.github.com>
2024-10-15 23:12:13 -0700

    update settings

--- NexMenu/settings/prod.py
+++ NexMenu/settings/prod.py
@@ -12,14 +12,6 @@ ALLOWED_HOSTS = ['*']
 
 ENVIRONMENT = 'production'
 
-print("DATABASE_URL: ", os.getenv('DATABASE_URL'))
-print("ENGINE: ", os.getenv('DB_ENGINE', default='django.db.backends.postgresql'))
-print("NAME: ", os.getenv('DB_NAME'))
-print("USER: ", os.getenv('DB_USER'))
-print("PASSWORD: ", os.getenv('DB_PASSWORD'))
-print("HOST: ", os.getenv('DB_HOST', default='localhost'))
-print("PORT: ", os.getenv('DB_PORT', default='5432'))
-
 DATABASES = {
     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
