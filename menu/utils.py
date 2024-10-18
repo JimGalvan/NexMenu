9e18527a8ea8cd6d5d1858e321ae207c61f77671
Jim Galvan <58128571+JimGalvan@users.noreply.github.com>
2024-10-17 23:59:21 -0700

    cache s3 connection

--- menu/utils.py
+++ menu/utils.py
@@ -1,9 +1,16 @@
 import boto3
 from django.conf import settings
 
+_cached_s3_client = None
+
 
 def get_s3_client():
-    return boto3.client('s3',
-                        aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-                        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-                        region_name=settings.AWS_S3_REGION_NAME)
+    global _cached_s3_client
+    if _cached_s3_client is None:
+        _cached_s3_client = boto3.client(
+            's3',
+            aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+            region_name=settings.AWS_S3_REGION_NAME
+        )
+    return _cached_s3_client
