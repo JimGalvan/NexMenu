b3b39b45296d77153f39441b4fd0d385e581ef87
Jim Galvan <58128571+JimGalvan@users.noreply.github.com>
2024-10-17 23:50:49 -0700

    fix settings

--- NexMenu/settings/prod.py
+++ NexMenu/settings/prod.py
@@ -7,6 +7,10 @@ from .base import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
+AWS_ACCESS_KEY_ID = str(os.getenv('AWS_ACCESS_KEY_ID'))
+AWS_SECRET_ACCESS_KEY = str(os.getenv('AWS_SECRET_ACCESS_KEY'))
+AWS_STORAGE_BUCKET_NAME = str(os.getenv('AWS_STORAGE_BUCKET_NAME'))
+AWS_S3_REGION_NAME = str(os.getenv('AWS_S3_REGION_NAME'))
 
 ALLOWED_HOSTS = ['*']
 
